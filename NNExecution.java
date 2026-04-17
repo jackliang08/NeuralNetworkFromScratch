1f420920a0b3157ddc7b6f38fd6215262df606c2
Duoyi Liang <jackliang2019@gmail.com>
2026-04-17 14:28:53 -0500

    Update NNExecution.java

--- NNExecution.java
+++ NNExecution.java
@@ -1,33 +1,85 @@
 package execution;
 import java.util.*;
-// TODO: figure out how to do multiple outputs
+import java.io.*;
+// TODO: figure out how to do multiple outputs (fixed update bias, retry multiple outputs)
 public class NNExecution {
 
-	public static void main(String[] args) {
-		double[] input1 ={1,1,1};
+	public static void main(String[] args) throws IOException{
+		Scanner reader = new Scanner(new File("src\\execution\\Iris.dat"));
+		String[] lineSplit;
 		int layers = 1;  // Change this to adjust layers
 		
 		NeuralNet nn1 = new NeuralNet(layers);
 		for (int i=0;i<layers-1;i++) {  // Adds the perceptrons to each layer
 			nn1.addPerceptrons(i,  3, 3);
 		}
-		nn1.addPerceptrons(layers-1,3,1);  // Adds the last hidden layer before output
-		nn1.addPerceptrons(layers, 1, 1);  // Adds the output perceptron
+		nn1.addPerceptrons(layers-1,4,3);  // Adds the last hidden layer before output
+		nn1.addPerceptrons(layers, 3, 1);  // Adds the output perceptron
 		
-		double[] o1,o2,o3;
+		double[] inputs;
 		double[] outputs;
-		for (int i=0;i<1000;i++) {
-			outputs = nn1.forwardAll(input1);
-			System.out.println(outputs[0]);
+		double[] targets; String target;
+		for (int i=0;i<150;i++) {
 			
-			System.out.println(Arrays.toString(nn1.perceptrons.get(nn1.perceptrons.size()-2).get(0).getWeights()));
-			nn1.backwardOutput(0.7, 0.6);
-			System.out.println(Arrays.toString(nn1.perceptrons.get(nn1.perceptrons.size()-2).get(0).getWeights()));
+			// Reads the data, then stores the information in inputs and outputs
+			if (!reader.hasNext()) break;
+			lineSplit = reader.nextLine().split("[,]");
+			
+			inputs = new double[lineSplit.length-1];
+			for (int inputInd=0;inputInd<lineSplit.length-1;inputInd++) {  // Stores the input data
+				inputs[inputInd] = Double.parseDouble(lineSplit[inputInd]);
+			}
+			
+			//Assigns output based on a one-hot vector system
+			target = lineSplit[lineSplit.length-1];
+			if (target.equals("Iris-setosa")) targets = new double[] {1,0,0};
+			else if (target.equals("Iris-versicolor")) targets = new double[] {0,1,0};
+			else if (target.equals("Iris-virginica")) targets = new double[] {0,0,1};
+			else targets = new double[] {0,0,0};
+			
+			
+			// Running the Neural Network for one epoch
+			System.out.printf("Epoch: %d%n", i);
+			outputs = nn1.forwardAll(inputs);
+			
+//			System.out.println(outputs[0]);
+//			System.out.println(outputs[1]);
+//			System.out.println(outputs[2]);
+			
+			//System.out.println(Arrays.toString(nn1.perceptrons.get(nn1.perceptrons.size()-2).get(0).getWeights()));
+			nn1.backwardOutput(0.1, targets);
+
+			//System.out.println(Arrays.toString(nn1.perceptrons.get(nn1.perceptrons.size()-2).get(0).getWeights()));
 			System.out.println();
 		}
-		//o2 = nn1.sigForward(1, o1);
+		// 6.5,3.0,5.5,1.8,Iris-virginica
+		outputs = nn1.forwardAll(new double[] {6.5,3.0,5.5,1.8});
 		
-
+		// Returns from one-hot to a categorical output
+		double[] oneHot = convertOneHot(outputs);
+		if (oneHot[0] == 1) {
+			System.out.println("Iris-setosa");
+		}
+		if (oneHot[1] == 1) {
+			System.out.println("Iris-versicolor");
+		}
+		if (oneHot[2] == 1) {
+			System.out.println("Iris-virginica");
+		}
+		System.out.println(Arrays.toString(outputs));
+	}
+	static double[] convertOneHot(double[] oneHot) {
+		double max=oneHot[0]; int maxIndex=0;
+		for (int i=0;i<oneHot.length;i++) {
+			if (oneHot[i] > max) {
+				max = oneHot[i];
+				maxIndex = i;
+			}
+		}
+		
+		double[] maxOneHot = new double[oneHot.length];
+		maxOneHot[maxIndex] = 1;
+		return maxOneHot;
 	}
 
 }
